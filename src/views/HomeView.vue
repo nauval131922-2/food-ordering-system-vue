c3f3f463ccecaa2d10a799fc9334162081d08268
Nauval Gunawan <nauvalgunawan5@gmail.com>
2025-01-23 20:06:17 +0700

    Fitur Login di Frontend | Vue JS + Laravel [8/18]

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
-<template>
-  <main>
-    <TheWelcome />
-  </main>
+<template lang="">
+  <div></div>
 </template>
+<script>
+import router from "@/router";
+
+export default {
+  data() {
+    return {
+      userName: "",
+    };
+  },
+
+  mounted() {
+    this.userName = localStorage.getItem("name");
+    if (!this.userName) {
+      this.$router.push({ name: "login" });
+    }
+  },
+};
+</script>
+<style lang=""></style>
